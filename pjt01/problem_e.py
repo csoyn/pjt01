24d63ddb7128a5a2690ac82add11c959f5b96c39
csoyn <csy51113@naver.com>
2021-01-22 21:21:59 +0900

    01.22. pjt 제출

--- pjt01/problem_e.py
+++ pjt01/problem_e.py
@@ -5,6 +5,19 @@ def dec_movies(movies):
     pass
     # 여기에 코드를 작성합니다.  
         
+    result_list=[ ]                          # 12월 개봉영화들 
+
+    for movie in movies:
+        movie_id = movie['id']                # movies 폴더에 있는 data를 불러오기 위해 id 변수 생성
+       
+        movies_json = open(f'data/movies/{movie_id}.json', encoding='UTF8')    
+        movies_list = json.load(movies_json)
+
+    if movies_list['release_date'][5:7] == 12: 
+        result_list.append(movies_list['original_title'])
+       
+    return result_list
+
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
